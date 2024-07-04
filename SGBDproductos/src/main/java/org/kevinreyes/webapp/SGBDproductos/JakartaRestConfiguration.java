7632fe1eb61ee031371953a74ba4a282f8c3df7b
Informatica <informatica@alumno.local>
2024-07-04 17:23:06 -0600

    web

---
+++ SGBDproductos/src/main/java/org/kevinreyes/webapp/SGBDproductos/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package org.kevinreyes.webapp.SGBDproductos;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
